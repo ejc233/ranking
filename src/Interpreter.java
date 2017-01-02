07bb197f073c92dab64631e97e0cb1ec4f29e773
ejc233 <ejc233@cornell.edu>
2017-01-02 02:02:48 -0800

    interpreter class started

---
+++ src/Interpreter.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Top-level class that accepts inputdir and outdir as command-line arguments
+ * and handles them appropriately. It reads the statements from the queries
+ * file.
+ * 
+ * @author echan
+ */
+public class Interpreter {
+	public static String inputFile;
+	public static String outputFile;
+
+	public static void main(String[] args) {
+		inputFile = args[0];
+		outputFile = args[1];
+		try {
+			BufferedReader inputReader = new BufferedReader(new FileReader(inputFile));
+
+			String line = null;
+			while ((line = inputReader.readLine()) != null) {
+				System.out.println(line + "hi");
+			}
+
+			inputReader.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
